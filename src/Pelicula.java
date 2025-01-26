cafa2d0aaa522cdb788eec3d985a131c1a40587c
Elvis Almonte <elvisalmonte14@gmail.com>
2025-01-26 11:40:12 -0400

    Creacion de instancia de la clase Pelicula

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -3,11 +3,22 @@ public class Pelicula {
     int fechaDeLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
+    double sumaDeLasEvaluaciones;
+    int totalDeLasEvaluaciones;
 
     void muestraFinchaTecnica(){
         System.out.println("El nombre de la pelicula es: " + nombre);
         System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
         System.out.println("Duracion en minutos: " + duracionEnMinutos);
+    }
+
+    void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones++;
+    }
+
+    double calculaMedia(){
+        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
     }
 
 
--- src/Principal.java
+++ src/Principal.java
@@ -4,8 +4,17 @@ public class Principal {
         miPelicula.nombre = "Encanto";
         miPelicula.fechaDeLanzamiento = 2021;
         miPelicula.duracionEnMinutos = 120;
-
+        //print ficha tecnica
         miPelicula.muestraFinchaTecnica();
+
+        miPelicula.evalua(10.0);
+        miPelicula.evalua(10.0);
+
+        //print
+        System.out.println(miPelicula.sumaDeLasEvaluaciones);
+        System.out.println(miPelicula.totalDeLasEvaluaciones);
+
+        System.out.println(miPelicula.calculaMedia());
 
         Pelicula otraPelicula = new Pelicula();
         otraPelicula.nombre = "Matrix";
@@ -13,6 +22,12 @@ public class Principal {
         otraPelicula.duracionEnMinutos = 180;
 
         otraPelicula.muestraFinchaTecnica();
+        otraPelicula.evalua(9.0);
+        otraPelicula.evalua(8.0);
+
+        System.out.println(otraPelicula.sumaDeLasEvaluaciones);
+        System.out.println(otraPelicula.totalDeLasEvaluaciones);
+        System.out.println(otraPelicula.calculaMedia());
 
     }
 }
